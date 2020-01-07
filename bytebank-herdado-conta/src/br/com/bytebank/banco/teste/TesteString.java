7e3c076364d07caec4d4aab97ae5ce6b981aa512
bianavic <bianavic@gmail.com>
2020-01-07 10:36:03 -0300

    java.lang

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,24 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+    public static void main(String[] args) {
+
+        // String é uma classe, o tipo,
+        // nome é uma referencia, criando um objeto.
+
+        String nome = "Alura"; // OBJECT LITERAL = uma String nao pode ser alterado
+        // imutabilidade. Uma x criado nao consegue alterar mais.
+        // a nao ser que vc crie uma NOVA String
+
+        // unico momento que nao precisa usar new para criar objeto
+        // String outro = new String("Alura");
+
+        // forma de alterar a String é criando outra
+        String outra = nome.replace("A", "a");
+
+        // nome.toLowerCase();
+        System.out.println(nome);
+        System.out.println(outra);
+
+    }
+}
